49ec4bc98c495cf671cd2c1bb43bb57c60c08fbd
JohnWelchDev <johnwelchdev@hotmail.co.uk>
2026-05-14 15:00:35 +0100

    initial check in created solution created Product entity

---
+++ KataCheckout.Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataCheckout.Entities
+{
+    /// <summary>
+    /// The product.
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Gets or sets the sku.
+        /// </summary>
+        public string? SKU { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unit price.
+        /// </summary>
+        public decimal UnitPrice { get; set; }
+    }
+}
